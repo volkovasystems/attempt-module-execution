4152e478cbae749b4f1e6d0d4c05a928e88e3385
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-08-14 07:08:28 +0800

    Major update. Finished how we handle results.

--- attempt-module-execution.js
+++ attempt-module-execution.js
@@ -40,54 +40,81 @@
 	@end-module-documentation
 
 	@include:
-	    {
-	        "extract-parameter-list-from-function.js@github.com/volkovasystems": "extractParameterListFromFunction",
-	        "camelize-namespace.js@github.com/volkovasystems": "camelizeNamespace",
-	        "path@nodejs": "path"
-	    }
+		{
+			"extract-parameter-list-from-function.js@github.com/volkovasystems": "extractParameterListFromFunction",
+			"camelize-namespace.js@github.com/volkovasystems": "camelizeNamespace",
+			"path@nodejs": "path"
+		}
 	@end-include
 */
 var attemptModuleExecution = function attemptModuleExecution( moduleMethod ){
 
-    var commandLineParameterList = process.argv;
-    var commandFilePath = commandLineParameterList[ 1 ];
+	var commandLineParameterList = process.argv;
+	var commandFilePath = commandLineParameterList[ 1 ];
 
-    var commandFileName = commandFilePath.split( path.sep ).reverse( )[ 0 ];
-    var commandMethodName = camelizeNamespace( commandFileName );
+	var commandFileName = commandFilePath.split( path.sep ).reverse( )[ 0 ];
+	var commandMethodName = camelizeNamespace( commandFileName );
 
-    if( moduleMethod.name != commandMethodName ){
-        var error = new Error( "fatal:method and command does not match" );
-        console.error( error );
-        throw error;
-    }
+	if( moduleMethod.name != commandMethodName ){
+		var error = new Error( "fatal:method and command does not match" );
+		console.error( error );
+		throw error;
+	}
 
-    var methodParameterList = extractParameterListFromFunction( moduleMethod );
+	var methodParameterList = extractParameterListFromFunction( moduleMethod );
 
-    var commandArgumentSet = parseCommandArgumentList( );
+	var commandArgumentSet = parseCommandArgumentList( );
 
-    var argumentValueList = [ ];
-    var parameterName = "";
+	var argumentValueList = [ ];
+	var parameterName = "";
 
-    var methodParameterListLength = methodParameterList.length;
-    for( var index = 0; index < methodParameterListLength; index++ ){
+	var methodParameterListLength = methodParameterList.length;
+	for( var index = 0; index < methodParameterListLength; index++ ){
 
-        parameterName = methodParameterList[ index ];
+		parameterName = methodParameterList[ index ];
 
-        if( parameterName in commandArgumentSet ){
-            argumentValueList[ index ] = commandArgumentSet[ parameterName ]
-        }
+		if( parameterName in commandArgumentSet ){
+			argumentValueList[ index ] = commandArgumentSet[ parameterName ]
+		}
 
-        if( index in commandArgumentSet ){
-            argumentValueList[ index ] = commandArgumentSet[ index ];
-        }
-    }
+		if( index in commandArgumentSet ){
+			argumentValueList[ index ] = commandArgumentSet[ index ];
+		}
+	}
 
-    var result = moduleMethod.apply( null, argumentValueList );
+	var callback = function callback( error, result ){
+		if( error ){
+			console.error( error );
 
-    if( typeof result != "undefined" ){
-        console.log(  )
-    }
+		}else{
+			if( typeof result == "string" ||
+				typeof result == "number" ||
+				typeof result == "boolean" )
+			{
+				console.log( result );
 
+			}else if( typeof result == "object" ){
+				try{
+					result = JSON.stringify( result );
+					result = new Buffer( util.inspect( result, { "depth": null } ) ).toString( "base64" );
+					result = "@transform-base64-to-json:" + result;
+
+					console.log( result );
+
+				}catch( error ){
+					console.error( error );
+				}
+
+			}else{
+				var error = new Error( "result cannot be stringify" );
+				console.error( error );
+			}
+		}
+	};
+
+	var result = moduleMethod.apply( null, argumentValueList );
+
+	callback( null, result );
 };
 
 var parseCommandArgumentList = require( "./parse-command-argument-list/parse-command-argument-list.js" );
@@ -95,5 +122,6 @@ var extractParameterListFromFunction = require( "./extract-parameter-list-from-f
 var camelizeNamespace = require( "./camelize-namespace/camelize-namespace.js" );
 
 var path = require( "path" );
+var util = require( "util" );
 
 module.exports = attemptModuleExecution;
